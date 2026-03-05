b38809b47a79c55092ba7cd15fd344093dd56b94
sahi seethal <seethalainavolu3@gmail.com>
2026-03-05 20:19:48 +0530

    Remove obsolete files and add new files

--- make_requirements.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-try:
-    with open('requirements.txt', 'w', encoding='utf-8') as f:
-        output = subprocess.check_output(['pip', 'freeze'], text=True)
-        f.write(output)
-    print("requirements.txt created successfully.")
-except Exception as e:
-    print(f"Error: {e}")
